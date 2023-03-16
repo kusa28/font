105191bad4336b2cccb1a245a53c6a865fdfcd3c
kusa28 <123868258+kusa28@users.noreply.github.com>
2023-03-16 12:49:11 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+const input = document.getElementById("input");
+const convertButton = document.getElementById("convert");
+const output = document.getElementById("output");
+
+// Google Fonts APIからフォントを読み込む
+WebFont.load({
+  google: {
+    families: ["Roboto", "Montserrat"]
+  }
+});
+
+convertButton.addEventListener("click", function() {
+  // テキストボックスからテキストを取得する
+  const text = input.value;
+
+  // ランダムにフォントを選択する
+  const fonts = ["Roboto", "Montserrat"];
+  const font = fonts[Math.floor(Math.random() * fonts.length)];
+
+  // 出力エリアのスタイルを設定する
+  output.style.fontFamily = font;
+  output.innerHTML = text;
+});
